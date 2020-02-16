b91f09737000093e932b47c169518f1ca57d68b8
LSafer <lsaferse@gmail.com>
2020-02-16 22:58:28 +0300

    Update FullBean.java
    added: forInstance(): creates a remote full-bean that is actually a remote to an instance that dose not implement FullBean.

--- src/main/java/cufy/beans/FullBean.java
+++ src/main/java/cufy/beans/FullBean.java
@@ -12,9 +12,12 @@
 package cufy.beans;
 
 import cufy.util.Object$;
+import cufy.util.Reflect$;
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * An implementation ready for the interface {@link Bean}. Increases teh ability to store un-fielded properties.
@@ -22,12 +25,82 @@ import java.util.Objects;
  * @param <K> the type of keys maintained by this map
  * @param <V> the type of mapped values
  * @author LSaferSE
- * @version 1 release (09-Dec-2019)
+ * @version 2 release (16-Feb-2020)
  * @since 09-Dec-2019
  */
 public interface FullBean<K, V> extends Bean<K, V> {
+	/**
+	 * Get a bean for the instance give. The given bean will work as a remote for the given instance. The bean will work as if the given instance is a
+	 * bean and returned bean is the actual instance.
+	 *
+	 * @param instance to get a bean for
+	 * @param <K>      the type of keys in the returned bean
+	 * @param <V>      the type of values in the returned bean
+	 * @return a bean remote for the given instance
+	 */
+	static <K, V> FullBean<K, V> forInstance(Object instance) {
+		return new FullBean<K, V>() {
+			/**
+			 * The properties set for this full bean.
+			 */
+			private BeanProperties<K, V> beanProperties;
+			@Override
+			public BeanProperties<K, V> getBeanProperties() {
+				if (this.beanProperties == null) {
+					this.beanProperties = new BeanProperties<>(this);
+					Set<K> keys = new HashSet<>(10);
 
+					Property property;
+					K key;
+					for (Field field : Reflect$.getAllFields(instance.getClass()))
+						if ((property = field.getAnnotation(Property.class)) != null && !keys.contains(key = this.getKey(field)))
+							this.beanProperties.add(new VirtualEntry<>(instance, this, field, property, key));
+				}
 
+				return this.beanProperties;
+			}
+			@Override
+			public VirtualEntry<K, V> getEntry(K key) {
+				BeanProperties<K, V> beanProperties = Objects.requireNonNull(this.getBeanProperties(), "getBeanProperties()");
+				Field field = this.getField(key);
+
+				if (field != null)
+					return this.getEntry(key, field);
+
+				return Object$.requireNonNullElseGet(beanProperties.get(key), () -> {
+					VirtualEntry<K, V> entry = new VirtualEntry<>(instance, this, beanProperties, key);
+					beanProperties.add(entry);
+					return entry;
+				});
+			}
+			@Override
+			public VirtualEntry<K, V> getEntry(K key, Field field) {
+				Objects.requireNonNull(field, "field");
+				Property property = Objects.requireNonNull(field.getAnnotation(Property.class), "field.getAnnotation(Property.class)");
+				BeanProperties<K, V> beanProperties = Objects.requireNonNull(this.getBeanProperties(), "getBeanProperties()");
+
+				return Object$.requireNonNullElseGet(beanProperties.get(key), () -> {
+					VirtualEntry<K, V> entry = new VirtualEntry<>(instance, this, beanProperties, field, property, key);
+					beanProperties.add(entry);
+					return entry;
+				});
+			}
+			@Override
+			public Field getField(K key) {
+				BeanProperties<K, V> beanProperties = Objects.requireNonNull(this.getBeanProperties(), "getBeanProperties()");
+				VirtualEntry<K, V> entry = beanProperties.get(key);
+
+				if (entry == null) {
+					for (Field field : Reflect$.getAllFields(instance.getClass()))
+						if (field.isAnnotationPresent(Property.class) && Objects.equals(key, this.getKey(field)))
+							return field;
+					return null;
+				} else {
+					return entry.field;
+				}
+			}
+		};
+	}
 	@Override
 	default VirtualEntry<K, V> getEntry(K key) {
 		BeanProperties<K, V> beanProperties = Objects.requireNonNull(this.getBeanProperties(), "getBeanProperties()");
